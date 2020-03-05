cf8e9ff389aa5487862c5dac676ac8d457298adc
ViktorKch <43659721+ViktorKch@users.noreply.github.com>
2020-03-05 16:23:55 +0700

    Update classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1jmRNj_IBaA9gxtR_oORdQCK6t1Pjte6m'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['hungry', 'needs burping', 'belly pain', 'discomfort', 'tired', 'lonely', 'cold/hot', 'scared', 'don\'t know', 'no worries']
 path = Path(__file__).parent
 
 app = Starlette()
