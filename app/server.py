00a8356d1ddf57460cef48db4460bef1e701ea21
ViktorKch <43659721+ViktorKch@users.noreply.github.com>
2020-03-06 12:16:54 +0700

    Update link

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1jmRNj_IBaA9gxtR_oORdQCK6t1Pjte6m'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-2itZu8KQGxF920AerB-k8KjDU9dOB_7'
 export_file_name = 'export.pkl'
 
-classes = ['hungry', 'needs burping', 'belly pain', 'discomfort', 'tired', 'lonely', 'cold/hot', 'scared', 'don\'t know', 'no worries']
+classes = ['crying', 'not_crying']
 path = Path(__file__).parent
 
 app = Starlette()
